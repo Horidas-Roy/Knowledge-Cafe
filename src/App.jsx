e896b32d8383356aaa75ce1a723733f98cd54a54
Horidas Roy <royhoridas64@gmail.com>
2023-09-14 20:32:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+import Header from './components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <div className='exo-2'>
+      <Header></Header>
+      <Blogs></Blogs>
+    </div>
+  )
+}
+
+export default App
